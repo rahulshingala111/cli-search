364f15ad3b1985290d8004af7b0b337184e17f35
rahul <43782508+rahulshingala111@users.noreply.github.com>
2026-06-17 19:35:24 +0530

    change in install script

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 set -e
 
 BINARY="search"
-DIR="~/.local/bin"
+DIR="$HOME/.local/bin"
 GITHUB="https://github.com/cli-search/releases/latest/download/$BINARY"
 echo "Downloading and Installing..."
-mkdir -p ~/.local/bin
+mkdir -p "~/.local/bin/$BINARY"
 curl -L $GITHUB -o "$DIR/$BINARY"
 chmod +x "$DIR/$BINARY"
 echo "Installed $BINARY tool in $DIR/$BINARY ....."
